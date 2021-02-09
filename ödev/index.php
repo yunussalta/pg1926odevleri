2dae5eebb58d3aba7ea4621b95b169426c9071df
yunussalta <76396184+yunussalta@users.noreply.github.com>
2021-02-09 22:49:09 +0300

    Delete ödev directory

--- ödev/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-     // require_once 'ödev21.php';
-    // require_once 'ödev.php';
-    //require_once 'ödev3.php';
-    //require_once 'asalsayi.php';
- 
-
-?>    
--- ödev/phyhton ödev 1.py
+++
@@ -1,9 +0,0 @@
-#python 4
-buyuk = []
-adet = int(input('Kaç Sayı Girilecek: '))
-for n in range(adet):
-    sayi = int(input('Sayıyı Gir: '))
-    buyuk.append(sayi)
-print("Liste İçindeki En Büyük Sayı :", max(buyuk))
-   
- 
